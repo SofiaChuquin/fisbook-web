29d424ccd50290e878ad39eaadca8c27ad97b970
Fabian Pierre Peña Jacobo <renzob130@gmail.com>
2017-05-31 22:35:15 -0500

    ADD: get token method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::SessionsController < ApplicationController
 
     def create
+        user = User.find_by_email(params[:user][:email])
+        if user.valid_password?(params[:user][:password])
+            user.get_token
+            render json: {error: {message: 'Authentication successfully.', user: user, status: 401}}, status: 401
+        else
+            puts 'Password is wrong'
+            render json: {error: {message: 'Authentication failed.', status: 401}}, status: 401
+        end
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable
+
+    def get_token
+        self.update_attribute(:token, SecureRandom.urlsafe_base64(nil, false)) if token.blank?
+        token
+    end
+
 end
