4ff382b8a79b939a3537a335320f456e0f15832a
Fabian Pierre Peña Jacobo <renzob130@gmail.com>
2017-05-31 23:07:26 -0500

    ADD: destroy method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -9,13 +9,23 @@ class Api::V1::SessionsController < ApplicationController
                 render json: {error: {message: 'Authentication failed. Unregistered user', status: 401}}, status: 401
             elsif user.valid_password?(params[:user][:password])
                 user.get_token
-                render json: {valid: {message: 'Authentication successfully.', user: user, status: 401}}, status: 401
+                render json: {valid: {message: 'Authentication successfully.', user: user, status: 200}}, status: 200
             else
                 puts 'Password is wrong'
                 render json: {error: {message: 'Authentication failed. Password is wrong', status: 401}}, status: 401
             end
         rescue Exception => e
             render json: {error: {message: "Error in Authentication. #{e}" , details: e.backtrace[0], status: 500}}, status: 500
+        end
+    end
+
+    def destroy
+        begin
+            user = User.find_by_email(params[:user][:email])
+            user.update_attribute(:token, nil)
+            render json: {valid: {message: 'Session destroyed'}, status: 200}, status: 200
+        rescue Exception => e
+            render json: {error: {message: "Error in logout. #{e}" , details: e.backtrace[0], status: 500}}, status: 500
         end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
     namespace :v1 do 
       namespace :sessions do
         post :create
+        delete :destroy
       end
       resources :notes
     end 
