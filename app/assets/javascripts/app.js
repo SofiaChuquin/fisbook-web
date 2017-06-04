cc4bc5d397e671e9bb5373e3a49e5f243af4ed6e
sofiachuquin <sofiachuquin@gmail.com>
2017-06-03 23:30:18 -0500

    Edit index view

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
     $('select').material_select();
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      draggable: true, // Choose whether you can drag to open on touch screens
+      -webkit-transform: translateX(0%),
+      transform: translateX(0%)
+    });
   });
+
+
