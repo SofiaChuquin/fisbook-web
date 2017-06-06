429b1a6eba12a7e339729fa02a98830b89dbc5a8
sofiachuquin <sofiachuquin@gmail.com>
2017-06-05 22:21:12 -0500

    Update executive view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,13 @@
 class HomeController < ApplicationController
-
-  def index
-    if current_person.person.rol == 'Estudiante'
-      cycle_id = current_person.enrollments.last.cycle_id
-      @courses = Course.where(cycle_id: cycle_id)
-    elsif current_person.person.rol == 'Docente'
-      teacher_id = current_person.id
-      @courses = Course.where(teacher_id: teacher_id)
-    elsif current_person.person.rol == 'Directivo'
-      @cycles = Cycle.all
-    end
-  end
+  # def index
+  #   if current_person.person.rol == 'Estudiante'
+  #     cycle_id = current_person.enrollments.last.cycle_id
+  #     @courses = Course.where(cycle_id: cycle_id)
+  #   elsif current_person.person.rol == 'Docente'
+  #     teacher_id = current_person.id
+  #     @courses = Course.where(teacher_id: teacher_id)
+  #   elsif current_person.person.rol == 'Directivo'
+  #     @cycles = Cycle.all
+  #   end
+  # end
 end
