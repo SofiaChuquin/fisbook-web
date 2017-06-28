10bd0e329c590dc6d1db2795bb57c570ccb8d152
sofiachuquin <sofiachuquin@gmail.com>
2017-06-28 16:45:56 -0500

    Edit seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
@@ -8,36 +5,86 @@
 
 Person.delete_all
 
+#Teachers
 person1 = Person.create(lastname: 'Hernandez Pujaico', name: 'Juan', dni: 12345676, phone: 123456798,
-                        email: 'juan@hotmail.com', gender: 'Masculino', state: 'Activo', rol: 'Directivo')
-person2 = Person.create(lastname: 'Hernandez Matta',name: 'Juancho', dni: 12342676, phone: 123451798,
-                        email: 'juancho@hotmail.com',gender: 'Masculino', state: 'Activo' , rol: 'Estudiante')
+                        email: 'juan@hotmail.com', gender: 'Masculino', state: 'Activo', rol: 'Docente')
+person2 = Person.create(lastname: 'Hernandez Matta',name: 'Jaime', dni: 12342676, phone: 123451798,
+                        email: 'jaime@hotmail.com',gender: 'Masculino', state: 'Activo' , rol: 'Docente')
 person3 = Person.create(lastname: 'Sotel Pujaico',name: 'Jose', dni: 12341676, phone: 121456798,
-                        email: 'jose@hotmail.com',gender: 'Masculino', state: 'Activo' , rol: 'Directivo')
-person4 = Person.create(lastname: 'Nuria Juila',name: 'Julia', dni: 12315676, phone: 123226798,
-                        email: 'julia@hotmail.com',gender: 'Femenino', state: 'Inactivo' , rol: 'Estudiante')
-person5 = Person.create(lastname: 'Hernandez Malta',name: 'Miguel', dni: 12344676, phone: 123956798,
-                        email: 'miguel@hotmail.com',gender: 'Femenino', state: 'Activo' , rol: 'Estudiante')
+                        email: 'jose@hotmail.com',gender: 'Masculino', state: 'Activo' , rol: 'Docente')
+#Students
+person4 = Person.create(lastname: 'Fernandez Ramirez',name: 'Miguel', dni: 12315676, phone: 123226798,
+                        email: 'miguel@hotmail.com',gender: 'Masculino', state: 'Inactivo' , rol: 'Estudiante')
+person5 = Person.create(lastname: 'Hernandez Mendez',name: 'Andrea', dni: 12344676, phone: 123956798,
+                        email: 'andrea@hotmail.com',gender: 'Femenino', state: 'Activo' , rol: 'Estudiante')
 person6 = Person.create(lastname: 'Palacios Pujaico',name: 'Juana', dni: 12342676, phone: 123456798,
                         email: 'juana@hotmail.com',gender: 'Femenino', state: 'Inactivo' , rol: 'Estudiante')
-person7 = Person.create(lastname: 'Carrillo Pujaico',name: 'Juliana', dni: 12340676, phone: 123456798,
-                        email: 'juliana@hotmail.com',gender: 'Femenino', state: 'Activo' , rol: 'Directivo')
+person7 = Person.create(lastname: 'Carrillo Pujaico',name: 'Julian', dni: 12340676, phone: 123456798,
+                        email: 'julian@hotmail.com',gender: 'Masculino', state: 'Activo' , rol: 'Estudiante')
+
+#Executives
 person8 = Person.create(lastname: 'Casas Pujaico',name: 'Maria', dni: 12348676, phone: 123456798,
-                        email: 'maria@hotmail.com',gender: 'Masculino', state: 'Inactivo' , rol: 'Docente')
+                        email: 'maria@hotmail.com',gender: 'Femenino', state: 'Inactivo' , rol: 'Directivo')
 person9 = Person.create(lastname: 'Peralta Pujaico',name: 'Omar', dni: 12349676, phone: 123356798,
-                        email: 'omar@hotmail.com',gender: 'Masculino', state: 'Activo' , rol: 'Docente')
-person10 = Person.create(lastname: 'Casita Pujaico',name: 'Oscar', dni: 12355676, phone: 122456798,
-                        email: 'oscar@hotmail.com',gender: 'Masculino', state: 'Inactivo' , rol: 'Docente')
+                        email: 'omar@hotmail.com',gender: 'Masculino', state: 'Activo' , rol: 'Directivo')
+person10 = Person.create(lastname: 'Casita Pujaico',name: 'Daniel', dni: 12355676, phone: 122456798,
+                        email: 'daniel@hotmail.com',gender: 'Masculino', state: 'Inactivo' , rol: 'Directivo')
 
-teacher1 = Teacher.create(people_id: person8.id, rank: 'Contratado')
-teacher2 = Teacher.create(people_id: person9.id, rank: 'Nombrado')
-teacher3 = Teacher.create(people_id: person10.id, rank: 'Contratado')
+teacher1 = Teacher.create(people_id: person1.id, rank: 'Contratado')
+teacher2 = Teacher.create(people_id: person2.id, rank: 'Nombrado')
+teacher3 = Teacher.create(people_id: person3.id, rank: 'Contratado')
 
-student1 = Student.create(people_id: person2.id, turn: 'Mañana')
-student2 = Student.create(people_id: person4.id, turn: 'Tarde')
-student3 = Student.create(people_id: person5.id, turn: 'Noche')
-student4 = Student.create(people_id: person6.id, turn: 'Mañana')
+student1 = Student.create(people_id: person4.id, turn: 'Mañana')
+student2 = Student.create(people_id: person5.id, turn: 'Tarde')
+student3 = Student.create(people_id: person6.id, turn: 'Noche')
+student4 = Student.create(people_id: person7.id, turn: 'Mañana')
 
-executive1 = Executive.create(people_id: person1.id, position: 'Secretario')
-executive2 = Executive.create(people_id: person3.id, position: 'Director')
-executive3 = Executive.create(people_id: person7.id, position: 'Secretario')
+executive1 = Executive.create(people_id: person8.id, position: 'Secretario')
+executive2 = Executive.create(people_id: person9.id, position: 'Director')
+executive3 = Executive.create(people_id: person10.id, position: 'Secretario')
+
+Cycle.delete_all
+
+cycle1 = Cycle.create(name: 'Primer Ciclo', semester: 'I', year: 2017)
+cycle2 = Cycle.create(name: 'Segundo Ciclo', semester: 'I', year: 2017)
+cycle3 = Cycle.create(name: 'Tercer Ciclo', semester: 'I', year: 2017)
+cycle4 = Cycle.create(name: 'Cuarto Ciclo', semester: 'I', year: 2017)
+cycle5 = Cycle.create(name: 'Quinto Ciclo', semester: 'I', year: 2017)
+cycle6 = Cycle.create(name: 'Sexto Ciclo', semester: 'I', year: 2017)
+cycle7 = Cycle.create(name: 'Séptimo Ciclo', semester: 'I', year: 2017)
+cycle8 = Cycle.create(name: 'Octavo Ciclo', semester: 'I', year: 2017)
+cycle9 = Cycle.create(name: 'Noveno Ciclo', semester: 'I', year: 2017)
+cycle10 = Cycle.create(name: 'Décimo Ciclo', semester: 'I', year: 2017)
+
+Course.delete_all
+
+course1 = Course.create(name: 'Inteligencia de Negocios', credits: 4, type_evaluation: 'A', hours: 5, teacher_id: teacher1.id, cycle_id: cycle9.id)
+course2 = Course.create(name: 'Desarrollo de Aplicaciones Empresariales', credits: 5, type_evaluation: 'A', hours: 6, teacher_id: teacher1.id, cycle_id: cycle9.id)
+course3 = Course.create(name: 'Sistemas Expertos', credits: 5, type_evaluation: 'B', hours: 4, teacher_id: teacher2.id, cycle_id: cycle9.id)
+course4 = Course.create(name: 'E-business', credits: 3, type_evaluation: 'C', hours: 5, teacher_id: teacher3.id, cycle_id: cycle9.id)
+
+Enrollment.delete_all
+
+enrollment1 = Enrollment.create(voucher: 'SIS123', active: true, student_id: student1.id, cycle_id: cycle9.id)
+enrollment2 = Enrollment.create(voucher: 'SIS345', active: true, student_id: student2.id, cycle_id: cycle9.id)
+enrollment3 = Enrollment.create(voucher: 'SIS678', active: true, student_id: student3.id, cycle_id: cycle9.id)
+enrollment4 = Enrollment.create(voucher: 'SIS951', active: true, student_id: student4.id, cycle_id: cycle9.id)
+
+Note.delete_all
+
+note1 = Note.create(first_practice: 11, second_practice: 12, third_practice: 13, first_exam: 11, second_exam: 13, permanent: 14, student_id: student1.id, course_id: course1.id)
+note2 = Note.create(first_practice: 11, second_practice: 12, third_practice: 13, first_exam: 11, second_exam: 13, permanent: 14, student_id: student1.id, course_id: course2.id)
+note3 = Note.create(first_practice: 11, second_practice: 12, third_practice: 13, first_exam: 11, second_exam: 13, permanent: 14, student_id: student1.id, course_id: course3.id)
+note4 = Note.create(first_practice: 11, second_practice: 12, third_practice: 13, first_exam: 11, second_exam: 13, permanent: 14, student_id: student1.id, course_id: course4.id)
+note5 = Note.create(first_practice: 11, second_practice: 12, third_practice: 13, first_exam: 11, second_exam: 13, permanent: 14, student_id: student2.id, course_id: course1.id)
+note6 = Note.create(first_practice: 11, second_practice: 12, third_practice: 13, first_exam: 11, second_exam: 13, permanent: 14, student_id: student2.id, course_id: course2.id)
+note7 = Note.create(first_practice: 11, second_practice: 12, third_practice: 13, first_exam: 11, second_exam: 13, permanent: 14, student_id: student2.id, course_id: course3.id)
+note8 = Note.create(first_practice: 11, second_practice: 12, third_practice: 13, first_exam: 11, second_exam: 13, permanent: 14, student_id: student2.id, course_id: course4.id)
+note9 = Note.create(first_practice: 11, second_practice: 12, third_practice: 13, first_exam: 11, second_exam: 13, permanent: 14, student_id: student3.id, course_id: course1.id)
+note10 = Note.create(first_practice: 11, second_practice: 12, third_practice: 13, first_exam: 11, second_exam: 13, permanent: 14, student_id: student3.id, course_id: course2.id)
+note11 = Note.create(first_practice: 11, second_practice: 12, third_practice: 13, first_exam: 11, second_exam: 13, permanent: 14, student_id: student3.id, course_id: course3.id)
+note12 = Note.create(first_practice: 11, second_practice: 12, third_practice: 13, first_exam: 11, second_exam: 13, permanent: 14, student_id: student3.id, course_id: course4.id)
+note13 = Note.create(first_practice: 11, second_practice: 12, third_practice: 13, first_exam: 11, second_exam: 13, permanent: 14, student_id: student4.id, course_id: course1.id)
+note14 = Note.create(first_practice: 11, second_practice: 12, third_practice: 13, first_exam: 11, second_exam: 13, permanent: 14, student_id: student4.id, course_id: course2.id)
+note15 = Note.create(first_practice: 11, second_practice: 12, third_practice: 13, first_exam: 11, second_exam: 13, permanent: 14, student_id: student4.id, course_id: course3.id)
+note16 = Note.create(first_practice: 11, second_practice: 12, third_practice: 13, first_exam: 11, second_exam: 13, permanent: 14, student_id: student4.id, course_id: course4.id)
